9493b3c2db6e22ea781e4c5d0db25cd1c0f5a76c
pitucorporacion-collab <pitucorporacion@gmail.com>
2026-08-19 12:28:38 -0300

    V2.3.1: exponer carga de imagenes de racks

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('monitorAPI', {
   openRfidPages: () => ipcRenderer.send('open-rfid-pages'),
   loadDeviceConfig: () => ipcRenderer.invoke('load-device-config'),
-  saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config)
+  saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config),
+  loadRackImage: (rack) => ipcRenderer.invoke('load-rack-image', rack)
 });
