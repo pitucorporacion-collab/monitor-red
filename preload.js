fd3198254b19cca82bce26f2acfdf30e0d22fd05
pitucorporacion-collab <pitucorporacion@gmail.com>
2026-08-19 07:38:52 -0300

    V2.3: persistencia de dispositivos

--- preload.js
+++ preload.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('monitorAPI', {
-  openRfidPages: () => ipcRenderer.send('open-rfid-pages')
+  openRfidPages: () => ipcRenderer.send('open-rfid-pages'),
+  loadDeviceConfig: () => ipcRenderer.invoke('load-device-config'),
+  saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config)
 });
