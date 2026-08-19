693aac9b3afdc0d5bd332a6f8086a18c5d33351b
pitucorporacion-collab <pitucorporacion@gmail.com>
2026-08-19 15:17:07 -0300

    Expone almacenamiento de racks al renderer

--- preload.js
+++ preload.js
@@ -4,6 +4,8 @@ contextBridge.exposeInMainWorld('monitorAPI', {
   openRfidPages: () => ipcRenderer.send('open-rfid-pages'),
   loadDeviceConfig: () => ipcRenderer.invoke('load-device-config'),
   saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config),
+  loadRackConfig: () => ipcRenderer.invoke('load-rack-config'),
+  saveRackConfig: (config) => ipcRenderer.invoke('save-rack-config', config),
   loadRackImage: (rack) => ipcRenderer.invoke('load-rack-image', rack),
   openRackImage: (rack) => ipcRenderer.invoke('open-rack-image', rack)
 });
