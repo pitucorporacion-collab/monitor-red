324ac1466b626d777c1e22b52d30b2c2d6d71353
pitucorporacion-collab <pitucorporacion@gmail.com>
2026-09-14 16:38:26 -0300

    Eliminar puente del sistema RFID externo

--- preload.js
+++ preload.js
@@ -1,7 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('monitorAPI', {
-  openRfidPages: () => ipcRenderer.send('open-rfid-pages'),
   loadDeviceConfig: () => ipcRenderer.invoke('load-device-config'),
   saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config)
 });
