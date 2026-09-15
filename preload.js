9d9625354a8d5bd1127fc7d4163f258724a9287d
pitucorporacion-collab <pitucorporacion@gmail.com>
2026-09-15 07:26:59 -0300

    Restaurar funciones de RACKS en preload

--- preload.js
+++ preload.js
@@ -2,5 +2,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('monitorAPI', {
   loadDeviceConfig: () => ipcRenderer.invoke('load-device-config'),
-  saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config)
+  saveDeviceConfig: (config) => ipcRenderer.invoke('save-device-config', config),
+  loadRackConfig: () => ipcRenderer.invoke('load-rack-config'),
+  saveRackConfig: (config) => ipcRenderer.invoke('save-rack-config', config),
+  loadRackImage: (rack) => ipcRenderer.invoke('load-rack-image', rack),
+  openRackImage: (rack) => ipcRenderer.invoke('open-rack-image', rack)
 });
